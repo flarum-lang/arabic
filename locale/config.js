f3ef9e3d7cbb5b96dc65c9e97180d8938bfb47b8
neerugupta <neeru.gupta@yahoo.com>
2021-07-07 17:58:15 +0530

    Update config.js

--- locale/config.js
+++ locale/config.js
@@ -1,6 +1,6 @@
-// source: https://unpkg.com/dayjs@v1.10.4/locale/xx.js
-// see: https://github.com/iamkun/dayjs/blob/v1.10.4/src/locale/xx.js
+// source: https://unpkg.com/dayjs@v1.10.4/locale/ar.js
+// see: https://github.com/iamkun/dayjs/blob/v1.10.4/src/locale/ar.js
 
-// TODO: copy compiled translations from https://unpkg.com/dayjs@v1.10.4/locale/xx.js
+// TODO: copy compiled translations from https://unpkg.com/dayjs@v1.10.4/locale/ar.js
 
-dayjs.locale('xx');
+dayjs.locale('ar');
